2956acd2849e6776346c131fd9dff56e4f11e561
ZeVictorAraujo <josearaujo.ba@hotmail.com>
2024-08-22 21:04:34 -0300

    Separação de testes unitários do código

--- calculadora.js
+++ calculadora.js
@@ -30,40 +30,11 @@ const calcular = () => {
         const [num1, operador, num2] = resposta.split(' ');
         const resultado = calculadora(parseFloat(num1), operador, parseFloat(num2));
         console.log(`Resultado: ${resultado}`);
-        interface(); // Volta ao menu principal após o cálculo
+        calcular();
     });
 };
 
-const verificaResultado = (esperado, real, mensagem) => {
-    if (esperado === real) {
-        console.log(`OK: ${mensagem} passou.`);
-    } else {
-        console.log(`ERRO: ${mensagem} falhou. Esperado ${esperado}, valor obtido ${real}`);
-    }
-};
 
-const testesUnitarios = () => {
-    verificaResultado(3, calculadora(1, '+', 2), "Testando soma");
-    verificaResultado(1, calculadora(3, '-', 2), "Testando subtração");
-    verificaResultado(6, calculadora(2, '*', 3), "Testando multiplicação");
-    verificaResultado(2, calculadora(6, '/', 3), "Testando divisão");
-    verificaResultado(8, calculadora(2, '^', 3), "Testando potenciação");
-};
 
-const interface = () => {
-    console.log("1 - Calcular");
-    console.log("2 - Teste Unitário");
-    rl.question('Digite a opção: ', (op) => {
-        if (op === '1') {
-            calcular();
-        } else if (op === '2') {
-            testesUnitarios();
-            interface();
-        } else {
-            console.log("Opção inválida. Tente novamente.");
-            interface();
-        }
-    });2
-};
 
-interface();
+calcular();
